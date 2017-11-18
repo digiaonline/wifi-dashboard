9ed3d0546599158881fa85910343144821f5a912
Sam Stenvall <sam.stenvall@nordsoftware.com>
2017-11-18 13:25:47 +0200

    Add skeleton Vue app

--- src/client/dashboard.js
+++ src/client/dashboard.js
@@ -1,8 +1,24 @@
+const Vue = require('vue');
+const Dashboard = require('../common/dashboard');
+
+// Configure the dashboard
+const dashboard = new Dashboard();
+
+// Configure the app
+const app = new Vue({
+  el: '#app',
+  data: {
+    dashboard: dashboard
+  }
+});
+
 // Open WebSocket connection
 const websocket = new WebSocket('ws://' + window.location.host);
 
 websocket.onmessage = function(event) {
-  const dashboard = JSON.parse(event.data);
+  // Update the dashboard
+  const updatedDashboard = JSON.parse(event.data);
 
-  console.log(dashboard);
+  dashboard.devices = updatedDashboard.devices;
+  console.log(updatedDashboard);
 };
